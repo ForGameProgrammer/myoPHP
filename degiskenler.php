e809684ad010e9756472886f1c9531cb97caac8c
ForGame <for123game@gmail.com>
2016-04-17 14:37:49 +0300

    get_all 1

---
+++ degiskenler.php
@@ -0,0 +1,11 @@
+<?php
+define("DB_TABLO","duyuru");
+define("DB_TABLO_ID","id");
+define("DB_TABLO_MESAJ","mesaj");
+define("DB_TABLO_TARIH","tarih");
+define("DB_TABLO_YAZAR","yazar");
+define("DB_HOST","localhost");
+define("DB_KULLANICI","forgamep_myo");
+define("DB_SIFRE","Kmyo_?123");
+define("DB_VERITABANI","forgamep_myo");
+?>
--- get_all.php
+++ get_all.php
@@ -1,1 +1,40 @@
 <?php
+require "degiskenler.php";
+$json = array();
+
+$db = @new mysqli(DB_HOST, DB_KULLANICI, DB_SIFRE, DB_VERITABANI);
+if ($db->connect_error)
+{
+    $json["basarili"] = 0;
+    $json["hata"] = "Veri Tabanına Bağlanılamadı.";
+    die(json_encode($json));
+}
+
+
+$tablo = DB_TABLO;
+$sorgu = $db->prepare("Select * from $tablo");
+$sorgu->execute();
+$sonuc = $sorgu->get_result();
+if ($sonuc->num_rows < 1)
+{
+    $json["basarili"] = 0;
+    $json["hata"] = "Gösterilecek Duyuru Yok.";
+    echo json_encode($json);
+}
+else
+{
+    $json["duyurular"] = array();
+    while ($row = $sonuc->fetch_array())
+    {
+        $duyuru = array();
+        $duyuru["id"] = $row["id"];
+        $duyuru["mesaj"] = $row["mesaj"];
+        $duyuru["tarih"] = $row["tarih"];
+        $duyuru["yazar"] = $row["yazar"];
+        array_push($json["duyurular"], $duyuru);
+    }
+    $json["basarili"] = 1;
+    echo json_encode($json);
+}
+
+?>
