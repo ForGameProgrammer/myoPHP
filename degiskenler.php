8d8ed3705d5755ebea9b8fc3d6cb46425421e291
ForGame <for123game@gmail.com>
2016-04-17 14:42:47 +0300

    Fixed DB Connection Veri Tabanı Bağlantısı Düzeltildi

--- degiskenler.php
+++ degiskenler.php
@@ -5,7 +5,7 @@ define("DB_TABLO_MESAJ","mesaj");
 define("DB_TABLO_TARIH","tarih");
 define("DB_TABLO_YAZAR","yazar");
 define("DB_HOST","localhost");
-define("DB_KULLANICI","forgamep_myo");
+define("DB_KULLANICI","forgamep_kmyo");
 define("DB_SIFRE","Kmyo_?123");
 define("DB_VERITABANI","forgamep_myo");
 ?>
