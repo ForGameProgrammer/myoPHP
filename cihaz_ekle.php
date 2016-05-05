df4bcbfc5b9c72baa03a2477aaaeeab9ccd7009b
ForGame <for123game@gmail.com>
2016-05-05 16:52:32 +0300

    Cihaz varsa Ekleme

--- cihaz_ekle.php
+++ cihaz_ekle.php
@@ -9,6 +9,17 @@ if (isset($_POST["cihaz"])) {
 
 	$cihaz = $_POST["cihaz"];
 
+	$sql = $db->prepare("SELECT * FROM " . TABLO_CIHAZLAR . " WHERE " . TABLO_CIHAZLAR_CIHAZID . "=?");
+	if (!$sql) {
+		die("SQL HATASI!");
+	}
+	$sql->bind_param("s",$cihaz);
+	$sql->execute();
+	$sonuc=$sql->get_result();
+	if ($sonuc->num_rows > 0) {
+		die("Zaten Var");
+	}
+
 	$sql = $db->prepare("INSERT INTO " . TABLO_CIHAZLAR . " (" . TABLO_CIHAZLAR_CIHAZID . ") VALUES(?)");
 	if (!$sql) {
 		die("SQL HATASI!");
