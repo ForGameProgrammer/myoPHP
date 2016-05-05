a183a00e65251e1c7a0465e4290add6651adf869
ForGame <for123game@gmail.com>
2016-05-05 19:04:49 +0300

    Duyurular İçin Notification Eklendi

--- duyuru_ekle.php
+++ duyuru_ekle.php
@@ -20,8 +20,69 @@ if (isset($_POST["mesaj"])) {
 	$sql->bind_param("ssss",$mesaj,$tarih,$yazar,$link);
 	$sql->execute();
 	echo "Duyurunuz Başarı ile Eklendi...";
+	sendMessageToAll($_POST["mesaj"]);
 
+	//This function will actually send the notification
+function sendNotification($registrationIds, $message)
+{
+    $msg = array
+    (
+        'duyuru' => $message,
+    );
+
+    $fields = array
+    (
+        'registration_ids' => $registrationIds,
+        'data' => $msg
+    );
+
+    $headers = array
+    (
+        'Authorization: key=' . API_ACCESS_KEYS,
+        'Content-Type: application/json'
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $res = json_decode($result);
+
+    $flag = $res->success;
+    if($flag >= 1){
+        echo "Duyuruları için Bildirimler Gönderildi...";
+    }else{
+        echo "Bildirimler Gönderilemedi...";
+    }
+}
+
+function sendMessageToAll($message){
+    $sql = $db->prepare("SELECT * FROM " . TABLO_CIHAZLAR);
+    if (!$sql) {
+        die("SQL HATASI!");
+    }
+    $sql->execute();
+    $sonuc=$sql->get_result();
+
+    $gcmRegIds = array();
+    while($query_row = $sonuc->fetch_array()) {
+         array_push($gcmRegIds, $query_row[TABLO_CIHAZLAR_CIHAZID]);
+    }
+    $pushMessage = $message;
+    if(isset($gcmRegIds) && isset($pushMessage)) {
+        $regIdChunk=array_chunk($gcmRegIds,1000);
+        foreach($regIdChunk as $RegId){
+            sendNotification($RegId, $pushMessage);
+        }
+
+    }
+}
 	$db->close();
-
 }
 ?>
